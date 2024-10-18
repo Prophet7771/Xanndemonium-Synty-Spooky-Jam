655191eace299d26a5fe380a20537fe154cb6203
Xander Kruger <xander.kruger7@gmail.com>
2024-10-18 21:13:30 +0200

    Function Library
    Created a Function Library to access any functionality anywhere without needing to duplicate code.
    Helps keep things clean.

---
+++ Project_Observer/Assets/Scripts/FunctionLibrary.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FL
+{
+    public static class FunctionLibrary { }
+
+    public static class MouseHandler
+    {
+        public static void ToggleCursor(bool value)
+        {
+            if (value)
+            {
+                Cursor.lockState = CursorLockMode.None; // Unlock the cursor
+                Cursor.visible = true; // Show the cursor again
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
+                Cursor.visible = false; // Hide the cursor
+            }
+        }
+    }
+}
