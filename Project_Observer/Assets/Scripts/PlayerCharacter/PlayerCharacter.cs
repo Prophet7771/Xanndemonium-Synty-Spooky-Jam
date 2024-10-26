8d127d3b7bb43bfad3dc5035241e9769779a4658
Xander Kruger <xander.kruger7@gmail.com>
2024-10-26 08:20:16 +0200

    Player Singleton
    Created singleton pattern for player character to be easily accessible for any object in the game.

---
+++ Project_Observer/Assets/Scripts/PlayerCharacter/PlayerCharacter.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    #region Variables
+
+    #region Singleton
+
+    public static PlayerCharacter Instance { get; private set; }
+
+    #endregion
+
+    [Header("Player Controller")]
+    PlayerController playerController;
+
+    #endregion
+
+    #region Start Functions
+
+    private void Awake()
+    {
+        // Check if an instance of the Player already exists
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject); // Destroy this instance if one already exists
+            return;
+        }
+
+        Instance = this; // Assign the correct instance of the player
+        DontDestroyOnLoad(gameObject); // Make sure it exists throughout all scenes
+
+        playerController = GetComponent<PlayerController>();
+    }
+
+    #endregion
+
+    #region Update Functions
+
+
+
+    #endregion
+
+    #region Base Functions
+
+
+
+    #endregion
+}
